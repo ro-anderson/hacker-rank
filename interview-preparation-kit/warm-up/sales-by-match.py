fc327e753c68bc7c67c4835ab84e9bd1f158d758
rodrigo.didier@semantix.com.br <rodrigo.didier@semantix.com.br>
2020-10-25 21:52:02 -0300

    started interviews train tests

---
+++ interview-preparation-kit/warm-up/sales-by-match.py
@@ -0,0 +1,45 @@
+import math
+import os
+import random
+import re
+import sys
+
+def count_freq_in_list(num, ar):
+    count = 0
+    for elem in ar:
+
+        if elem == num:
+            count +=1
+    return count
+
+def get_dict_with_freqs(ar):
+        dict_freq = {num: count_freq_in_list(num, ar) for num in set(ar)}
+        return dict_freq
+
+def btw1and100(n):
+    if n >=1 and n <= 100:
+        return True
+    return False
+
+def check_ar(ar):
+    for elem in ar:
+        if not btw1and100(elem):
+            return False
+    return True
+            
+def sockMerchant(n, ar):
+    # constrains
+    if btw1and100(n) and check_ar(ar):
+        dict_freq = get_dict_with_freqs(ar) 
+        pairs_per_element = [x // 2 for x in dict_freq.values()]
+        return sum(pairs_per_element) 
+
+if __name__ == '__main__':
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+    print(result)
+	
