1a9ecbb89633fc920e17118e63d2f4247e5de80d
rodrigo.didier@semantix.com.br <rodrigo.didier@semantix.com.br>
2020-11-30 21:55:17 -0300

    add code

--- interview-preparation-kit/warm-up/counting_valleys.py
+++ interview-preparation-kit/warm-up/counting_valleys.py
@@ -4,7 +4,6 @@ import random
 import re
 import sys
 
-
 def btw2and10e6(steps):
     if steps >= 2 and steps <= 1000000:
         return True
@@ -24,48 +23,52 @@ def step_value(step):
     else:
         return -1
 
-def can_be_valley(path, idx):
-    if path[idx] == 0 and path[idx + 1] == -1:
+def can_be_valley(levels, idx):
+    if levels[idx] == 0 and levels[idx + 1] == -1 and idx + 1 < len(levels):
         return True
     return False
 
-def is_valley(path, idx):
+def is_valley(levels, idx):
     count = idx + 1
-    while count < len(path):
-        if path[count] == 0:
+    while count < len(levels):
+        if levels[count] == 0:
             return  True
+        count += 1
     return False
 
 def list_of_levels(path):
     idx = 0
-    levels = []
+    levels = [0]
 
     for step in list(path):
         if idx == 0:
-            levels.append(0)
+            #levels.append(0)
             levels.append(step_value(step))
 
-        elif idx + 1 < len(path):
+        elif idx < len(path):
             levels.append(levels[idx] + step_value(step))
         idx += 1
     return levels
-path = 'UDDDDDD'
-levels = list_of_levels(path)
+
 
 def countingValleys(steps, path):
     levels = list_of_levels(path)
     idx = 0
     valleys = 0
-    for level in levels:
-        if can_be_valley(path,idx):
-            if is_valley(path, idx):
+    for idx in range(len(levels)):
+        if can_be_valley(levels,idx):
+            if is_valley(levels, idx):
                 valleys += 1
-        idx += 1
     return valleys
 
 if __name__ == '__main__':
+
+    # path = 'UDDDDDD'
+    # levels = list_of_levels(path)
     steps = int(input().strip())
     path = input()
 
     if btw2and10e6(steps) and valid_path(path):
+        levels = list_of_levels(path)
+        #print(levels)
         print(countingValleys(steps, path))
